588a3f771e28d05d58792f027268b5a8c1c6114f
caovanhoang <89053040+caovanhoanggg@users.noreply.github.com>
2021-12-03 20:39:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "I LOVE YOU",
+    introTitle: 'NGUYỄN THỊ TUYẾT NHUNG <3',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Nhấn vào đây đi',
+    title: 'Phải chăng anh đã yêu em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Bỏ qua bước lựa chọn này thì bạn chính là người yêu của mình :3 ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà. Yêu bạn Nhung nhiều nhiều 😘😘',
+    messDesc: 'Hẹn bạn , mình sẽ dắt bạn đi ăn 1 ngày không xa <3',
+    btnAccept: 'Oke bạn <3',
+    messLink: 'https://m.me/1057789150'
+}
